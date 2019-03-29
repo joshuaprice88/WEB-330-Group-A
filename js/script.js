877aeefa1fda8cc7670de9053fdde30326a9e737
“tkarmstrong” <tkarmstrong@my365.bellevue.edu>
2019-03-28 21:32:17 -0500

    Changed font family, centered Jumbotron, and various other minor changes

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
   
-   //Name Modal button id="startGame" is clicked will triger startContent to hide and gameContent to show
+   //Name Modal button id="startGame" is clicked will trigger startContent to hide and gameContent to show
    $("#startGame").click(function(){
       $("#startContent").hide();
       $("#gameContent").show();
@@ -17,8 +17,8 @@ $(document).ready(function(){
       
       //check to see it answer has been selected if yes go to next step if no return to question.
       //pull answer input and check if correct return true or false
-      //if true add 1 to correct asnwered variable to display correct answers to user
-      //if false add 1 to incorrect asnwered variable to display correct answers to user
+      //if true add 1 to correct answered variable to display correct answers to user
+      //if false add 1 to incorrect answered variable to display correct answers to user
 
       //close modal
       $('#questionModal').modal('toggle');
